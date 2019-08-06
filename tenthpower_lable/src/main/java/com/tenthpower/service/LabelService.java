9928d5a6b29af1e94e1c22c1fd1c90e780f5b06d
hyp <17621421619@163.com>
2019-08-06 21:25:15 +0800

    增加lable 的分页搜索查询，

--- tenthpower_lable/src/main/java/com/tenthpower/service/LabelService.java
+++ tenthpower_lable/src/main/java/com/tenthpower/service/LabelService.java
@@ -5,9 +5,17 @@ import com.tenthpower.dto.lable.LabelVo;
 import com.tenthpower.pojo.Label;
 import com.tenthpower.util.BeanCopierEx;
 import com.tenthpower.util.IdWorker;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -65,4 +73,50 @@ public class LabelService {
     public void deleteById(String id){
         labelDao.deleteById(id);
     }
+
+    /**
+     * 搜索
+     */
+    public List<LabelVo> findSearch(LabelVo labelVo){
+        Specification specification= createSpecification(labelVo);
+        return labelDao.findAll(specification);
+    }
+
+    /**
+     * 分页加搜索
+     */
+    public Page<LabelVo> findSearch(LabelVo labelVo, int page, int size){
+        Specification specification= createSpecification(labelVo);
+        PageRequest pageRequest = PageRequest.of(page-1,size);
+        return labelDao.findAll(specification ,pageRequest);
+    }
+
+    /**
+     * 构建查询条件
+     * @param labelVo
+     * @return
+     */
+    private Specification<Label> createSpecification(LabelVo labelVo){
+        return new Specification<Label>() {
+            @Override
+            public Predicate toPredicate(Root<Label> root, CriteriaQuery<?>
+                    criteriaQuery, CriteriaBuilder cb) {
+                List<Predicate> predicateList=new ArrayList<>();
+                if (StringUtils.isNotBlank(labelVo.getLabelname())) {
+                    predicateList.add(cb.like(root.get("labelname").as(String.class),"%"+labelVo.getLabelname()+"%"));
+                }
+                if(labelVo.getState() != null){
+                    predicateList.add(cb.equal(root.get("state").as(Integer.class),labelVo.getState()));
+                }
+                if(labelVo.getState() != null){
+                    predicateList.add(cb.equal(root.get("state").as(Integer.class),labelVo.getState()));
+                }
+                if(labelVo.getRecommend() != null){
+                    predicateList.add(cb.equal(root.get("recommend").as(Integer.class),labelVo.getRecommend()));
+                }
+                return cb.and(predicateList.toArray(new Predicate[predicateList.size()]));
+            }
+        };
+    }
+
 }
